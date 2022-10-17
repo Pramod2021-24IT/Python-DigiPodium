e244f62cfbd7b7d70705e2d3acab5adcfc2b2842
Pramod Kumar Maurya <95674009+Pramod2021-24IT@users.noreply.github.com>
2022-10-17 18:20:04 +0530

    I/O

---
+++ Example.py
@@ -0,0 +1,4 @@
+name=input("Enter your name :")
+print('Welcome',name)
+amount=input("How much tea will you take?")
+print("Why would you want",amount,"tea?")
---
+++ ascii.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main(){
+    for(int n=1;n<150;n++){
+        printf("%c\n",n);
+    }
+}
